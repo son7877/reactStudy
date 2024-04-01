86579c272de5a3320fa0b610cee2d2045bc47de3
son7877 <hongbeom7877@naver.com>
2024-04-01 11:38:03 +0900

    reactStudy

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,21 +3,29 @@ import { useState } from "react";
 function Counter() {
   const [i, setI] = useState(0);
   const [input, setInput] = useState(0);
+  const [logs, setLogs] = useState([]);
+
   const plus = () => {
-    setI(i + Number(input));
-    setInput("");
+    setI(i + input);
+    setInput(Number(""));
+    // 기존 로그에 새 로그 추가
+    // map에 대한 고유한 키 권장
+    setLogs([...logs, `${i} + ${Number(input)} = ${i + Number(input)}`]);
   };
   const minus = () => {
     setI(i - Number(input));
     setInput("");
+    setLogs([...logs, `${i} - ${Number(input)} = ${i - Number(input)}`]);
   };
   const multiple = () => {
     setI(i * Number(input));
     setInput("");
+    setLogs([...logs, `${i} * ${Number(input)} = ${i * Number(input)}`]);
   };
   const division = () => {
     setI(i / Number(input));
     setInput("");
+    setLogs([...logs, `${i} / ${Number(input)} = ${i / Number(input)}`]);
   };
 
   return (
@@ -25,13 +33,16 @@ function Counter() {
       <h1>{i}</h1>
       <input
         placeholder="숫자를 입력해주세요"
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => setInput(Number(e.target.value))}
         type="number"
       />
-      <button onClick={() => plus()}>+</button>
-      <button onClick={() => minus()}>-</button>
-      <button onClick={() => multiple()}>*</button>
-      {input != 0 && <button onClick={() => division()}>/</button>}
+      <button onClick={plus}>+</button>
+      <button onClick={minus}>-</button>
+      <button onClick={multiple}>*</button>
+      {input != 0 && <button onClick={division}>/</button>}
+      {logs.map((str, i) => {
+        return <p key={i}>{str}</p>;
+      })}
     </div>
   );
 }
